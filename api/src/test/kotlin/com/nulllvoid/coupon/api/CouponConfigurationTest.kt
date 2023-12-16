1d54c3688122a8e220f613146179155b35d2acff
nulllvoid <shivam.shah@cogoport.com>
2023-12-16 16:34:11 +0530

    feat: added tests

--- api/src/test/kotlin/com/nulllvoid/coupon/api/CouponConfigurationTest.kt
+++ api/src/test/kotlin/com/nulllvoid/coupon/api/CouponConfigurationTest.kt
@@ -1,6 +1,10 @@
 
 package com.nulllvoid.coupon.api
 
+import com.nullvoid.coupon.api.coupon.entity.CouponConfiguration
+import com.nullvoid.coupon.api.coupon.repository.couponConfiguration.CouponConfigRepository
+import com.nullvoid.coupon.model.Configuration
+import com.nullvoid.coupon.model.request.CreateConfigurationRequest
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
@@ -20,10 +24,12 @@ import java.util.UUID
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @MicronautTest(transactional = false)
-class CouponConfigurationTest {
+class CouponConfigurationTest(
+    private val couponConfigRepository: CouponConfigRepository
+) {
 
     @Inject
-    @field:Client("/config")
+    @field:Client("/coupon")
     lateinit var client: HttpClient
     @BeforeEach
     fun setUp() = runTest {
@@ -34,19 +40,34 @@ class CouponConfigurationTest {
     }
 
     @Test
-    fun canCreateNewItems() = runTest {
-        val endpoint = "/item"
-        val creator = UUID.randomUUID()
+    fun canAddConfiguration() = runTest {
+        val createConfigurationRequest = CreateConfigurationRequest(
+            couponCode = "test",
+            configuration = Configuration(
+                userDailyRepeatCount = 1,
+                userWeeklyRepeatCount = 1,
+                userTotalRepeatCount = 2,
+                globalTotalRepeatCount = 10000
+            )
+        )
+        val endpoint = "/configuration"
         val request = HttpRequest.POST(
             endpoint,
-
-            ""
+            createConfigurationRequest
         )
 
         // Non-existing item should be created
         val response: HttpResponse<Any> = withContext(Dispatchers.IO) {
             client.toBlocking().exchange(request)
         }
-        Assertions.assertEquals(HttpStatus.CREATED, response.status)
+        val expectedResponse = CouponConfiguration(
+            couponCode = createConfigurationRequest.couponCode,
+            userTotalRepeatCount = createConfigurationRequest.configuration.userTotalRepeatCount,
+            userWeeklyRepeatCount = createConfigurationRequest.configuration.userWeeklyRepeatCount,
+            userDailyRepeatCount = createConfigurationRequest.configuration.userDailyRepeatCount,
+            globalTotalRepeatCount = createConfigurationRequest.configuration.globalTotalRepeatCount
+        )
+        Assertions.assertEquals(HttpStatus.OK, response.status)
+        Assertions.assertEquals(couponConfigRepository.findByCouponCode(createConfigurationRequest.couponCode),expectedResponse)
     }
 }
